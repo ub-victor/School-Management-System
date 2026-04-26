8aeb0cd8ed33a9273c3c31e5c423f402ead36d9b
ub-victor <victoireushindi371@gmail.com>
2026-04-26 23:39:35 +0200

    Initial commit

---
+++ src/com/school/model/Person.java
@@ -0,0 +1,10 @@
+package com.school.model;
+
+/**
+ * Abstract base class representing a person in in the school.
+ * Demostrates abstraction and encapsulation.
+ */
+public abstract class Person {
+    
+    
+}
