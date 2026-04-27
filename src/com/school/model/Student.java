360206587e7715d33025cf1c5cdf2ca841245949
ub-victor <victoireushindi371@gmail.com>
2026-04-27 14:50:28 +0200

    Initial commit

--- src/com/school/model/Student.java
+++ src/com/school/model/Student.java
@@ -3,6 +3,10 @@ package com.school.model;
 public class Student extends Person {
     private double feesPaid;
     private double totalFees;
-
-        
+    
+    public Student(String id, String name, String email, double feesPaid, double totalFees) {
+        super(id, name, email);
+        this.feesPaid = feesPaid;
+        this.totalFees = totalFees;
+    }   
 }
