1976b44a0097394f9eb2ce6d50c335d9cad70ff8
ub-victor <victoireushindi371@gmail.com>
2026-04-27 14:51:23 +0200

    Initial commit

--- src/com/school/model/Student.java
+++ src/com/school/model/Student.java
@@ -8,5 +8,23 @@ public class Student extends Person {
         super(id, name, email);
         this.feesPaid = feesPaid;
         this.totalFees = totalFees;
-    }   
+    }
+
+    public double getFeesPaid() {
+        return feesPaid;
+    }
+
+    public void setFeesPaid(double feesPaid) {
+        this.feesPaid = feesPaid;
+    }
+
+    public double getTotalFees() {
+        return totalFees;
+    }
+
+    public void setTotalFees(double totalFees) {
+        this.totalFees = totalFees;
+    }  
+    
+    
 }
