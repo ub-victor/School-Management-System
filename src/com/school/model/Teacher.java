429ad413527d56ab026163d14a317c1f50dcb7d3
ub-victor <victoireushindi371@gmail.com>
2026-05-03 17:21:09 +0200

    Initial Commit

--- src/com/school/model/Teacher.java
+++ src/com/school/model/Teacher.java
@@ -1,6 +1,16 @@
 package com.school.model;
 
 public class Teacher extends Person{
+    private String subjectSpecialization;
+    private double salary;
+
+    public Teacher(String id, String name, String email, String subjectSpecialization, double salary) {
+        super(id, name, email);
+        this.subjectSpecialization = subjectSpecialization;
+        this.salary = salary;
+    }
+
+
 
     @Override
     public String getRole() {
