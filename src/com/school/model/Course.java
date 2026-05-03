67bced2d3c8e82686ef85f7b280e21436336642a
ub-victor <victoireushindi371@gmail.com>
2026-05-03 17:42:23 +0200

    Initial Commit

--- src/com/school/model/Course.java
+++ src/com/school/model/Course.java
@@ -11,14 +11,7 @@ public class Course {
     private List<Student> enrolledStudents; // Composition (list of Student)
     private int maxCapacity;
     
-    public Course(String courseCode, String courseName, Teacher teacher, List<Student> enrolledStudents,
-            int maxCapacity) {
-        this.courseCode = courseCode;
-        this.courseName = courseName;
-        this.teacher = teacher;
-        this.enrolledStudents = enrolledStudents;
-        this.maxCapacity = maxCapacity;
-    }
+    
 
     
 
