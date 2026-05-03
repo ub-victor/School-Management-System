66b66fdf965d5ab637a208bc077634212a412b80
ub-victor <victoireushindi371@gmail.com>
2026-05-03 17:27:19 +0200

    Initial Commit

--- src/com/school/model/Teacher.java
+++ src/com/school/model/Teacher.java
@@ -33,7 +33,7 @@ public class Teacher extends Person{
 
     @Override
     public String toString() {
-        return super.toString()+ String.format();
+        return super.toString()+ String.format(" | Specialization: %s | Salary: $%.2f", subjectSpecialization, salary);
     }
 
     
