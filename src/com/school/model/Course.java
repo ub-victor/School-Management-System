d8aa72effb272b6cc2531910927d47c1d615515e
ub-victor <victoireushindi371@gmail.com>
2026-05-03 17:41:20 +0200

    Initial Commit

--- src/com/school/model/Course.java
+++ src/com/school/model/Course.java
@@ -10,9 +10,18 @@ public class Course {
     private Teacher teacher;  // Association  with Teacher
     private List<Student> enrolledStudents; // Composition (list of Student)
     private int maxCapacity;
-
+    
+    public Course(String courseCode, String courseName, Teacher teacher, List<Student> enrolledStudents,
+            int maxCapacity) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.teacher = teacher;
+        this.enrolledStudents = enrolledStudents;
+        this.maxCapacity = maxCapacity;
+    }
 
     
+
 
 
 }
